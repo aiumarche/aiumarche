d6ff3176179008565e3a9c94abccdaae129f3d98
Ouka Suzuki <ouka@MacBook-Air-3.local>
2026-02-18 11:58:20 +0900

    footer

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -129,7 +129,7 @@ export default function RootLayout({
             </nav>
 
             <div style={{ borderTop: '1px solid rgba(255,255,255,0.15)', paddingTop: '30px', fontSize: '0.7rem', color: '#9fb3a8' }}>
-              © AIU Marché. All Rights Reserved.
+              © AIU Marché. 
             </div>
           </div>
         </footer>
